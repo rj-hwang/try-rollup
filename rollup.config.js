44e28b9a4b9bb3d584f799ef7a7ed6d7da7d7cd0
RJ <rongjihuang@gmail.com>
2019-06-17 23:47:34 +0800

    TODO

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,47 @@
 import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+//import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
 
-export default {
-  entry: 'src/main.js',
-  format: 'cjs',
-  plugins: [json(), babel()],
-  //dest: 'dist/bundle.js',
-  targets: [
-    { dest: 'dist/bundle.es.js', format: 'es' },
-    { dest: 'dist/bundle.amd.js', format: 'amd' },
-    { dest: 'dist/bundle.cjs.js', format: 'cjs' },
-    { dest: 'dist/bundle.iife.js', format: 'iife', moduleName: 'main' },
-    { dest: 'dist/bundle.umd.js', format: 'umd', moduleName: 'main' }
-  ],
-  sourceMap: true,  // default false, true|false|'inline'
-  globals: {jquery: "$"},
-  external: ["jquery"],
-  indent: false,
-  banner: '/* banner */',
-  footer: '/* footer */',
-  intro: '/* intro */',
-  outro: '/* outro */'
-};
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/main.js',
+    output: {
+      name: "tryRollup",
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      json(),
+      resolve(), // so Rollup can find `ms`
+      //commonjs() // so Rollup can convert `ms` to an ES module
+      babel({
+        exclude: 'node_modules/**' // only transpile our source code
+      })
+    ]
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/main.js',
+    //external: ['ms'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      json(),
+      resolve(),
+      babel({
+        exclude: 'node_modules/**' // only transpile our source code
+      })
+    ]
+  }
+];
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { version } from '../package.json';
-import m1 from "./m1";
-import {add} from "./m2";
-import * as $ from "jquery"; // external
 
-console.log(m1);
-console.log(add(1, 2));
-console.log($);
-export default () => console.log(m1 + add(2 + 3));
+let i = 1;
+const j = 2
+
+export default (n = 1) => console.log(version + " - " + (n + i + j));
